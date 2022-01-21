b78c038145755f9fb87348e9dbb9a2728d52271f
Adam Smaka <adam@smaka.dev>
2022-01-21 16:06:10 +0200

    Build simple UI

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -9,7 +9,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weather'),
+        title: const Text('Pogoda'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '-5',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Text(
+              'degree',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: 60),
+            Text(
+              'London',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            const SizedBox(height: 60),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text('City'),
+                        hintText: 'London',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Get'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
