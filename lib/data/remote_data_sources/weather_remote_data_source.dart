a4db37ed00919dd7957cc28609b1de8ca520b84d
Adam Smaka <adam@smaka.dev>
2022-01-21 23:46:09 +0200

    Fix displaying dio error

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -8,8 +8,14 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherJson({
     required String city,
   }) async {
-    final response = await _httpClient.get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=48bc1821aeb54a88a98134716222101&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await _httpClient.get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=48bc1821aeb54a88a98134716222101&q=$city&aqi=no');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+        error.response?.data['error']['message'] ?? 'Unknown http error',
+      );
+    }
   }
 }
