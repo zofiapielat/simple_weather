9c8e77150e33fd4169e912bc2c6d2f274ad501a9
ZofiaPielat <z.pielat@gmail.com>
2023-10-03 11:50:25 +0200

    Implement Dio

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=5a3cbccfbed2410e92b152326232609&q=$city&aqi=no ');
+
+    final responseData = response.data;
+
+    if (responseData == null) {
+      return null;
+    }
+
+    final name = responseData['location']['name'] as String;
+    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+
+    return WeatherModel(city: name, temperature: temperature);
   }
 }
